6aea2f5af9e6637a4a9bf40dbd89cca62d6c6371
Vishwaraj Anand <7369612+vishwarajanand@users.noreply.github.com>
2019-09-13 14:52:10 +0530

    Adding better text in images which can help classify image number vs product number

--- README.md
+++ README.md
@@ -3,10 +3,17 @@ Contains script to create bulk images using python with numbers written on them 
 
 I had created this repo so that I have a tool to create several images for FB catalog testing.
 
-
 # Usage
 `python create_multiple_images.py` - change configs and function params to het different results
+OR
+`python3 create_multiple_images.py` - change configs and function params to het different results
 
-# Depemndency on PIL (Python Image Library)
-Install as below:
+# Dependencies
+
+There is a dependency on PIL (Python Image Library):
+
+## If using Python 2.7:
 `pip install PIP`
+
+## If using Python3.5+:
+`python3 -m pip install Pillow`
--- create_multiple_images.py
+++ create_multiple_images.py
@@ -1,4 +1,3 @@
-
 import os
 from PIL import Image, ImageDraw, ImageFont
 
@@ -11,39 +10,58 @@ TotalImageColumns = 10
 Width = 700
 Height = 700
 
+def getStringifiedNumber(x):
+    switcher = {
+    1: str(x)+"st",
+    2: str(x) + "nd",
+        3: str(x) + "rd",
+        0: str(x) + "th",
+    }
+    switcher_selector = abs(x)% 10
+    if switcher_selector > 3:
+        switcher_selector = 0
+    return switcher.get(switcher_selector)
+
+
+def getImageName(i,j):
+    return getStringifiedNumber(i) + " image of " + getStringifiedNumber(j) + " product."
+
+
 def createImage(name):
-	# create Image object with default size
-	image = Image.new('RGB', (Width, Height), color = 'red')
+    # create Image object with default size
+    image = Image.new('RGB', (Width, Height), color='red')
 
-	# or create Image object with default image in cwd
-	# image = Image.open('default.png')
+    # or create Image object with default image in cwd
+    # image = Image.open('default.png')
 
-	# initialize the drawing context with the image object as background
-	draw = ImageDraw.Draw(image)
+    # initialize the drawing context with the image object as background
+    draw = ImageDraw.Draw(image)
 
-	# create font object with the font file and specify desired size
-	font = ImageFont.truetype('Roboto-Bold.ttf', size=45)
+    # create font object with the font file and specify desired size
+    font = ImageFont.truetype('Roboto-Bold.ttf', size=45)
 
-	# starting position of the message
+    # starting position of the message
+    (x, y) = (50, 50)
+    message = "Demo Image"
+    color = 'rgb(0, 0, 0)'  # black color
 
-	(x, y) = (50, 50)
-	message = "Demo Image"
-	color = 'rgb(0, 0, 0)' # black color
+    # draw the message on the background
+    draw.text((x, y), message, fill=color, font=font)
+    (x, y) = (150, 150)
+    color = 'rgb(255, 255, 255)'  # white color
+    draw.text((x, y), name, fill=color, font=font)
 
-	# draw the message on the background
-	draw.text((x, y), message, fill=color, font=font)
-	(x, y) = (150, 150)
-	color = 'rgb(255, 255, 255)' # white color
-	draw.text((x, y), name, fill=color, font=font)
+    # save the edited image
+    image.save(name + '.png')
 
-	# save the edited image
-	image.save(name+'.png')
 
-print ("Creating " + str(TotalImageRows*TotalImageColumns) + " images.")
-for i in range(1,TotalImageRows+1):
-	for j in range(1,TotalImageColumns+1):
-		createImage(str(i)+"-"+str(j));
-		print ("Created image number: "+str(i)+"-"+str(j))
+print("Creating " + str(TotalImageRows * TotalImageColumns) + " images.");
+for i in range(1, TotalImageRows + 1):
+    for j in range(1, TotalImageColumns + 1):
+        image_name = getImageName(i,j)
+        createImage(image_name)
+        print("Created image number: " + image_name)
 
-print ("Images are located at: " + os.getcwd())
-print ("You may upload them on GitHub to get hosted internet URLs.")
+
+print("Images are located at: " + os.getcwd())
+print("You may upload them on GitHub to get hosted internet URLs.")
